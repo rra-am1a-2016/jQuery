5a2e7c33535fc2d1388a9e675674cab6a837f166
Arjan de Ruijter <rra@mboutrecht.nl>
2016-12-01 09:14:45 +0100

    Initial commit

---
+++ js_code.js
@@ -0,0 +1,41 @@
+//alert("Deze code staat in een apart bestand!")
+
+// We gaan nu aan de slag met JQuery
+
+$(document).ready(function(){
+  
+  $("#btn_01").click(function()
+  {
+      // Selecteer alle p-tags, deze manier heet chaining
+      $("p").css("background-color","green")
+            .css("width", "200px")
+            .css("border", "5px solid yellow");
+
+      // Toepassen van css d.m.v een javascript object
+      var cssValues = {
+         "font-family": "Verdana",
+         "font-size": "1.2em"
+      };
+
+      $("p").css(cssValues);
+
+      // Zet een javascript-object direct in de css method
+      $("p").css({"color": "white", "border-radius": "0.5em"});
+
+      var cssPTags = {"padding": "0.5em",
+                        "outline-style": "double",
+                        "outline-color": "red",
+                        "outline-width": "1em"};
+      $("p").css(cssPTags);
+  });
+
+  $("p").click(function(){
+     $(this).hide();
+  });
+
+  $("#btn_02").click(function(){
+     $("p").show();
+  });
+
+
+});

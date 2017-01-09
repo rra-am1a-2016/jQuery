c0b566566b08886e4d24759ddca0b498c750f24f
Arjan de Ruijter <rra@mboutrecht.nl>
2017-01-09 11:56:20 +0100

    before after

--- jquery_add.js
+++ jquery_add.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 
 
-   $('button').click(function(){
+   $('button').eq(0).click(function(){
       // Maak een random geheel getal tussen de 0 en de lengte van het fruit-array
       var index = Math.floor(Math.random() * fruit.length);
 
@@ -13,14 +13,14 @@ $(document).ready(function(){
       if ( choise == "append")
       {
          // Voeg een listItem toe
-         $("ol").append("<li>" + fruit[index] + "</li>");
+         $("ol").eq(0).append("<li>" + fruit[index] + "</li>");
       }
       else if (choise == "prepend")
       {
-         $("ol").prepend("<li>" + fruit[index] + "</li>");
+         $("ol").eq(0).prepend("<li>" + fruit[index] + "</li>");
       }
       else{
-         $("h3").text("Kies append of prepend").animate({"height": "30px",
+         $("h3").eq(0).text("Kies append of prepend").animate({"height": "30px",
                                                          "border-width": "2px",
                                                          "background-color": "yellow"}, 1000, "easeOutBounce");
       }
@@ -34,4 +34,25 @@ $(document).ready(function(){
          $(this).remove();
       });
    });
+
+   var ictThings = ["jQuery", "Javascript", "HTML5", "CSS", "SQL", "PHP", "JSON", "AJAX"];
+
+   $("button").eq(1).click(function(){
+
+      var index1 = Math.floor(Math.random() * ictThings.length); 
+
+      
+
+      var choice1 = $("input[type=radio][name=choiseI]:checked").val();
+
+      if (choice1 == "after")
+      {
+            $("ol:last li:last").after("<li>" + ictThings[index1] + "</li>");
+      }
+      else if (choice1 == "before")
+      {
+            $("ol:last li:first").before("<li>" + ictThings[index1] + "</li>");         
+      }
+   });
+
 });
